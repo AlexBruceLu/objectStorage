dd361df6592c7d238094b9ad570aceb63b7f7734
AlexBruceLU <lu_1000_yu@163.com>
2019-11-15 19:43:01 +0800

    单机版基于http的对象存储

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"object/objects"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/objects/", objects.Handler)
+	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
+}
