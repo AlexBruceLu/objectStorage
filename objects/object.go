1ef80c3fd21f40a4f67d2034fdfe99db42dcbd4e
root <root@DESKTOP-4DVTL16.localdomain>
2022-02-26 15:59:20 +0800

    release v0.1

---
+++ objects/object.go
@@ -0,0 +1,49 @@
+package objects
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const STORAGE_ROOT = "storage_data"
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		get(w, r)
+	case http.MethodPut:
+		put(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("%s/objects/%s", STORAGE_ROOT, strings.Split(r.URL.EscapedPath(), "/")[2])
+	f, e := os.Open(path)
+	if e != nil {
+		log.Println(e)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("%s/objects/%s", STORAGE_ROOT, strings.Split(r.URL.EscapedPath(), "/")[2])
+	f, e := os.Create(path)
+	if e != nil {
+		log.Println(e)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(f, r.Body)
+}
